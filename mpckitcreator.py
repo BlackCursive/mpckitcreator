edb9c94a3d5ae5dc2370ff8c427b7483ce2a5afb
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-10-28 13:40:18 -0400

    Update mpckitcreator.py

--- mpckitcreator.py
+++ mpckitcreator.py
@@ -1,5 +1,6 @@
 import os
 from soundfile import SoundFile
+import xml.etree.ElementTree as ET
 
 directory = '/content/drive/MyDrive/Colab Notebooks/wav_converter/'
 os.chdir(directory)
@@ -14,6 +15,8 @@ sf.available_subtypes('WAV')
 
 print("Return the default subtype for a given format.\n")
 sf.default_subtype('WAV')
+
+### List files from directory with Resolution, Channel, Sample Rate and Number of Sample ### 
 
 for filename in os.listdir(directory):
   if filename.endswith(".WAV") or filename.endswith(".wav"):
@@ -31,3 +34,34 @@ for filename in os.listdir(directory):
         print(f"Channels = Stereo \n")
       else:
         print(f"Channels = Mono \n")
+        
+### Import MPC *.xpm file and figure out XML Structure ### 
+
+tree = ET.parse('mpc.xpm')
+root = tree.getroot()
+
+print(root)
+print(root[0])
+print(root[1])
+
+for i in root[1]:
+  print(i.tag, i.attrib)
+
+for i in root[0][1]:
+  print(i.text)
+  
+print(f"Root - {root}\n")
+print(f"File Version - {root[0][0].text}\n")
+print(f"Application_Version - {root[0][1].text}\n")
+print(f"Program Name - {root[1][0].text}\n")
+print(f"Element - {root[1][22].tag}\n")
+print(f"Instrument - {root[1][22][0].tag}\n")
+print(f"Insrument Number - {root[1][22][0].attrib}\n")
+print(f"Layers - {root[1][22][0][66].tag}\n")
+print(f"Layer - {root[1][22][0][66][0].tag}\n")
+print(f"Layer Number - {root[1][22][0][66][0].attrib}\n")
+print(f"Layer Number - {root[1][22][0][66][1].attrib}\n")
+print(f"SampleName - {root[1][22][0][66][0][18].tag}\n")
+print(f"SampleName Text- {root[1][22][0][66][0][18].text}\n")
+print(f"SliceEnd - {root[1][22][0][66][0][24].tag}\n")
+print(f"SlicEnd Text- {root[1][22][0][66][0][24].text}\n")
