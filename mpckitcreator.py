2155651d773b3f78211fe090ab6b513d0897d13b
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-10-28 13:31:33 -0400

    Create mpckitcreator.py

---
+++ mpckitcreator.py
@@ -0,0 +1,24 @@
+import os
+from soundfile import SoundFile
+
+directory = '/content/drive/MyDrive/Colab Notebooks/wav_converter/'
+os.chdir(directory)
+
+# directory = os.getcwd()
+
+for filename in os.listdir(directory):
+  if filename.endswith(".WAV") or filename.endswith(".wav"):
+    with SoundFile(filename, 'r') as myfile:
+      print(f"Filename = {myfile.name}")
+      print(f"Number of Samples = {myfile.frames-1}")
+      print(f"Sample Rate = {myfile.samplerate} Hz")
+      if myfile.subtype == 'PCM_16':
+        print(f"Resolution = 16 bits")
+      if myfile.subtype == 'PCM_24':
+        print(f"Resolution = 24 bits")
+      if myfile.subtype == 'PCM_32' or  myfile.subtype == 'FLOAT':
+        print(f"Resolution = 32 bits")
+      if myfile.channels == 2:
+        print(f"Channels = Stereo \n")
+      else:
+        print(f"Channels = Mono \n")
