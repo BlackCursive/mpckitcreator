fb42d2bebd573301e44ab096ae27ac830641e8e0
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-11-06 18:17:54 -0500

    Create read_smpl_mpckit.py

---
+++ read_smpl_mpckit.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from bs4 import BeautifulSoup
+from subprocess import call
+
+xpm = sys.argv[1]
+
+with open(xpm, 'r') as f:
+    data = f.read()
+
+soup = BeautifulSoup(data, "xml")
+
+def clear():
+    # check and make call for specific operating system
+    _ = call('clear' if os.name == 'posix' else 'cls')
+clear()
+
+def read_smpl_list(xpm):
+  count=0
+  for tag in soup.find_all('SampleName'):
+    if len(tag.get_text(strip=True)) != 0:
+        # print(f"i+1} Sample Name : {tag.text} / Slice End : {slcEnd}")
+        count +=1
+        print(f"{count} - {tag.text}")
+read_smpl_list(xpm)
