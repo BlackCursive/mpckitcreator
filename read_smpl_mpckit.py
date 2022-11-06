1240d5465e8c3ddc344a48f8dd102b690badd890
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-11-06 18:20:55 -0500

    Update read_smpl_mpckit.py

--- read_smpl_mpckit.py
+++ read_smpl_mpckit.py
@@ -11,10 +11,10 @@ with open(xpm, 'r') as f:
 soup = BeautifulSoup(data, "xml")
 
 def clear():
-    # check and make call for specific operating system
     _ = call('clear' if os.name == 'posix' else 'cls')
 clear()
 
+### Function to get Filename and Number of Samples from a Program kit
 def read_smpl_list(xpm):
   count=0
   for tag in soup.find_all('SampleName'):
