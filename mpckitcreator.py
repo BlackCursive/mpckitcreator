edb3a37bd5ac7d9864eb4f6e255593aadb9b4929
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-11-08 15:08:28 -0500

    Update mpckitcreator.py

--- mpckitcreator.py
+++ mpckitcreator.py
@@ -1,33 +1,24 @@
 import os
-import shutil
-from read_wav_files import sample_list
-from read_wav_files import no_of_frames
+from read_wav_files import *
+from templates import *
 
 directory_path = os.getcwd()
 folder_name = os.path.basename(directory_path)
 
-### Copy Blank Template File to New File With Folder Name
-newFilename = folder_name + ".xpm"
-shutil.copyfile("template.xml", newFilename)
-
 ### Write Program Name
-with open(newFilename, "r") as templateFile:
-  template = templateFile.read()
-with open(newFilename, "w") as file:
-    file.write(template.replace('__PROGRAMNAME__', folder_name))
+programName = template.replace('__PROGRAMNAME__', folder_name)
 
 ### Write all Wav Files and Number of Samples to instruments
-with open("instrument.xml", "r") as instrumentFile:
-  instrument = instrumentFile.read()
+stringList = []
 for count, x, y in zip(range(len(sample_list)),sample_list, no_of_frames):
-  with open('instrument_full.xml', "a") as file:
-    file.write(instrument.replace('__SAMPLENAME__', x).replace('__SLICEEND__', str(y)).replace('__INSTNUMBER__', str(count+1)))
-    file.write("\n")
+  stringList.append(instruments.replace('__SAMPLENAME__', x).replace('__SLICEEND__', str(y)).replace('__INSTNUMBER__', str(count+1)))
 
 ### Write instruments to Program
-with open(newFilename, "r") as newProgram:
-  newProgramKit = newProgram.read()
-with open('instrument_full.xml', "r") as testXML:
-  testXMLFile = testXML.read()
+instrumentAndLayers = programName.replace('__INSTRUMENTSANDLAYERS__', '\n'.join(stringList))
+
+### Create New File With Folder Name
+newFilename = folder_name + ".xpm"
+
+### Create New MPC Program
 with open(newFilename, "w") as newProgramKitFile:
-  newProgramKitFile.write(newProgramKit.replace('__INSTRUMENTSANDLAYERS__', testXMLFile))
+  newProgramKitFile.write(instrumentAndLayers)
