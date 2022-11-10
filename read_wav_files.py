ad02046e7e06a3434c762b598436388dc43c4461
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-11-09 21:15:43 -0500

    Update read_wav_files.py

--- read_wav_files.py
+++ read_wav_files.py
@@ -13,9 +13,9 @@ def wavFolder(folder):
     if file.endswith(".WAV") or file.endswith(".wav"):
       with SoundFile(file, 'r') as myfile:
         count +=1
-        filename = myfile.name
-        sample_list.append(filename)
+        filename = myfile.name.split('.')
+        sample_list.append(filename[0])
         frames = myfile.frames-1
         no_of_frames.append(frames)
-        # print(f"{count} - Sample Name: {filename} / No. of Frames: {frames}")
+        # print(f"{count} - Sample Name: {filename[0]} / No. of Frames: {frames}")
 wavFolder(directory_path)
